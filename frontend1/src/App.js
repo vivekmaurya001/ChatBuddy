185302848e0b2831d48bab6fba1a75bfb389bd4d
vivekmaurya001 <155618190+vivekmaurya001@users.noreply.github.com>
2024-04-15 15:03:16 +0530

    Update App.js

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./components/Homepages/HomePage";
 import Chatpage from "./components/Chatpage/Chatpage";
 import "./App.css";
 import VideoCall from "./components/micelenous/VideoCall";
+import MainSitePage from "./components/Homepages/MainSitePage";
 function App() {
   return (
     <div
@@ -12,11 +13,11 @@ function App() {
         height: "100vh",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
       }}
     >
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/" element={<MainSitePage />} />
+        <Route exact path="/login" element={<HomePage />} />
         <Route exact path="/chats" element={<Chatpage />} />
         <Route exact path="/room/:RoomId" element={<VideoCall />} />
       </Routes>
